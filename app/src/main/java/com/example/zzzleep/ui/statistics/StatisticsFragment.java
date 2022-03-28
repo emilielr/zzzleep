65e92c661096eca6ca144ef343e3f2aa2c6875c2
emilielr <43408175+emilielr@users.noreply.github.com>
2022-03-28 16:13:13 +0200

    Display correct data in chart and style correct according to figma

--- app/src/main/java/com/example/zzzleep/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/zzzleep/ui/statistics/StatisticsFragment.java
@@ -1,7 +1,6 @@
 package com.example.zzzleep.ui.statistics;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,10 +14,12 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.zzzleep.R;
 import com.example.zzzleep.databinding.FragmentStatisticsBinding;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -50,25 +51,68 @@ public class StatisticsFragment extends Fragment {
         ArrayList<SleepObject> data = getFileData();
         getBarEntries(data);
 
-        // creating a new bar data set.
-        barDataSet = new BarDataSet(barEntriesArrayList, "Sleep pattern");
+        barDataSet = new BarDataSet(barEntriesArrayList, "Hours of sleep");
 
-        // creating a new bar data and passing our bar data set.
         barData = new BarData(barDataSet);
 
         barChart.setData(barData);
+        barChart.invalidate();
 
-        // adding color to our bar data set.
-        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
-        // setting text color.
-        barDataSet.setValueTextColor(Color.BLACK);
-
-        // setting text size
-        barDataSet.setValueTextSize(16f);
-        barChart.getDescription().setEnabled(false);
+        initBarDataSet(barDataSet);
+        initBarChart();
 
         return root;
+    }
+
+    // function for stying the outlook of the bar chart
+    private void initBarDataSet(BarDataSet barDataSet){
+        barDataSet.setColors(R.color.dark_blue);
+        //showing the value of the bar, default true if not set
+        barDataSet.setDrawValues(false);
+        //setting the text size of the value of the bar
+        barDataSet.setValueTextSize(16f);
+        barDataSet.setValueTextColor(R.color.black);
+        barChart.getDescription().setEnabled(false);
+        barData.setBarWidth(0.9f);
+        barChart.setFitBars(true);
+    }
+
+    private void initBarChart() {
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawGridLines(false);
+
+        YAxis rightAxis = barChart.getAxisRight();
+        rightAxis.setDrawAxisLine(false);
+        rightAxis.setDrawGridLines(true);
+        rightAxis.setAxisMinimum(0);
+        rightAxis.setAxisMaximum(10);
+        rightAxis.setGridLineWidth(1f);
+        rightAxis.setGranularity(2f);
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setDrawAxisLine(false);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLabels(false);
+        leftAxis.setAxisMinimum(0);
+        leftAxis.setAxisMaximum(10);
+        leftAxis.setGranularity(2f);
+
+        Legend legend = barChart.getLegend();
+        //setting the shape of the legend form to line, default square shape
+        legend.setForm(Legend.LegendForm.LINE);
+        //setting the text size of the legend
+        legend.setTextSize(11f);
+        //setting the alignment of legend toward the chart
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        //setting the stacking direction of legend
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        //setting the location of legend outside the chart, default false if not set
+        legend.setDrawInside(false);
+
     }
 
 
